dfc5144d1136457a5f4cd98307565aff629260d1
Pawel Piotrowicz <pawel.piotrowicz@intel.com>
2021-01-08 14:48:55 +0000

    save performance

--- dot_reduce/main.cpp
+++ dot_reduce/main.cpp
@@ -31,6 +31,16 @@ struct Content {
   size_t reduce_size;
   size_t getSize() { return Out.size(); }
 
+  Content(size_t _size, size_t _reduce)
+  {
+    reduce_size = _reduce;
+    Out.resize(_size);
+    Left.resize(_size * _reduce);
+    Right.resize(Left.size());
+    std::fill(Out.begin(), Out.end(), 0);
+    std::fill(Left.begin(), Left.end(), 0);
+    std::fill(Right.begin(), Right.end(), 0);
+  }
   Content(const std::vector<T>& _L, const std::vector<T>& _R, size_t _reduce_size=3)  {
 
            if( (_L.size() != _R.size()) || !_L.size() || !_R.size() )
@@ -64,6 +74,14 @@ std::ostream& operator<<(std::ostream& o, const Content<T>& c) {
 }
 
 
+size_t min_div(size_t in) {
+
+    for(size_t i=2;i<64;i++)
+      if(in % i == 0) return i;
+
+  return 1;
+}
+
 template<class T>
 void run_case()
 {
@@ -73,6 +91,17 @@ void run_case()
   v.push_back(Content<T>({0.5, 2.1, 3.1}, {0.5, 2.2, 3.2}, 1));
   v.push_back(Content<T>({1.0, 2.0, 3.0}, {1.0, 2.0, 3.0}, 3));
   v.push_back(Content<T>({1.0, 2.0, 3.0, 1.0, 2.0, 3.0}, {1.0, 2.0, 3.0, 1.0, 2.0, 3.0}, 3));
+
+  for(size_t dim=10;dim<512;dim+=32)
+  {
+    for(size_t reduce=2;reduce<64;reduce+=2)
+    {
+        Content<T> c(dim,reduce);
+        v.push_back(c);
+        cpugraph::GenerateRandomData((T *)c.Left.data(),dim*reduce);
+        cpugraph::GenerateRandomData((T *)c.Right.data(),dim*reduce);
+    }
+  }
 
   for (auto &item : v)
   {
@@ -92,14 +121,26 @@ void run_case()
 
     for(size_t i=0;i<CopyGccOutput.size();i++)
     {
-      log_info("CopyGccOutput{"<<CopyGccOutput[i] <<"} item.Out{"<<item.Out[i] <<"}");
+     // log_info("CopyGccOutput{"<<CopyGccOutput[i] <<"} item.Out{"<<item.Out[i] <<"}");
       if (!cpugraph::AreSame<T>(CopyGccOutput[i], item.Out[i]))
       {
         log_err("type=" << cpugraph::getTypeName<T>::name()  << " Not equal vec_size=" << CopyGccOutput.size() << " reduction=" << item.reduce_size << "  CopyGccOutput[" << i << "] != My[" << i << "] => {" << CopyGccOutput[i] << " != " << item.Out[i] << "}");
       }
     }
 
-    log_info("OK :) "<< "type=" << cpugraph::getTypeName<T>::name() << " vec_size{L,R}=" << item.Right.size() << " and reduction=" << item.reduce_size );
+    std::vector<std::pair<int64_t, std::string>> result{
+        {diff_duration_my.count(), "my"},
+        {diff_duration_reduce_gcc.count(), "gcc"}
+       };
+
+    std::sort(result.begin(), result.end(), [](std::pair<int64_t, std::string> &p1, std::pair<int64_t, std::string> &p2) { return p1.first < p2.first; });
+
+    auto best = result[0].first;
+    auto worst = result[1].first;
+    int gain = (int)((((double)worst / (double)best) - 1) * 100);
+
+    log_info("OK :) "
+             << "type=" << cpugraph::getTypeName<T>::name() << " vec_size{L,R}=" << item.Right.size() << " and reduction=" << item.reduce_size << cpugraph::join(result, [](std::pair<int64_t, std::string> &in) { return in.second; },4) << " times=" << cpugraph::join(result, [](std::pair<int64_t, std::string> &in) { return in.first; },5) << " gain=" << gain << "%" );
    }
 
 }
